05d7d428559fb57f16f16690f86db942575e78cf
akikasaki <alektramp@gmail.com>
2016-12-12 21:28:21 +0100

    Mala Ispravka

--- github.com/PaulThomasAnderson/S2S.git/S2S/src/Izvrsavanje.java
+++ github.com/PaulThomasAnderson/S2S.git/S2S/src/Izvrsavanje.java
@@ -1,16 +1,16 @@
 import java.util.LinkedList;
 
 public class Izvrsavanje {
-	public void prikaziSveStudente(LinkedList studenti){
-		if(studenti==null){
+	public void prikaziSveStudente(LinkedList<Student1> studenti) {
+		if (studenti == null) {
 			System.out.println("GReska");
 			return;
-			
+
 		}
 		int i = 0;
-		while(i<studenti.size()){
-		System.out.println(studenti.get(i).toString());
-		i++;
+		while (i < studenti.size()) {
+			System.out.println(studenti.get(i).toString());
+			i++;
 		}
 	}
 }
