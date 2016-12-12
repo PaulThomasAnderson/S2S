3a990544c7edcc41b603fe7123381f5d3a59cbfe
akikasaki <alektramp@gmail.com>
2016-12-12 21:26:01 +0100

    Napravljena klasa izvrsi

---
+++ github.com/PaulThomasAnderson/S2S.git/S2S/src/Izvrsavanje.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+public class Izvrsavanje {
+	public void prikaziSveStudente(LinkedList studenti){
+		if(studenti==null){
+			System.out.println("GReska");
+			return;
+			
+		}
+		int i = 0;
+		while(i<studenti.size()){
+		System.out.println(studenti.get(i).toString());
+		i++;
+		}
+	}
+}
